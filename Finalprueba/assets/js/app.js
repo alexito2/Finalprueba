dc1caaf39035f30fb24b9eb7ffe118b17494392a
alexito2 <85403880+alexito2@users.noreply.github.com>
2022-03-10 21:38:11 -0600

    Delete Finalprueba directory

--- Finalprueba/assets/js/app.js
+++
@@ -1,9 +0,0 @@
-
-//confirmar si soporta sw
-if ( navigator.serviceWorker ){
-    //alert("Soporta el SW");
-    navigator.serviceWorker.register("/sw.js");
-}
-else{
-    alert(":( no soporta el SW");
-}
